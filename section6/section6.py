8608255aa6dee669d144c88add7fce823037cbc0
tsusil2019 <tsusil2019@gmail.com>
2019-03-26 10:39:27 +0530

    1stchange

---
+++ section6/section6.py
@@ -0,0 +1,35 @@
+
+# 37 NUMBER AND MATHS
+
+""" + Addition
+- Subtraction
+* Multiplication
+/ Division
+** Exponentiation
+% Modulo
+// Integer Division
+ """
+
+print(1 + 3) #4
+print(40-4.5) #35.5
+print(1/2) # 0.5
+print(2**2) #4
+print(4%2) #0
+print(4//2) # 2
+print(1+2*5/3)
+
+
+   # NOTE
+    #  1 Parentheses
+    #  2 Exponents
+    #  3 Multiplication
+    #  4 Division
+    #  5 Addition
+    #  6 Subtraction 
+
+
+#===========================================================================
+    #38 COMMENTS
+    # Single line charater
+    # """ """ Multiple 
+
---
+++ section7/section7.py
@@ -0,0 +1,67 @@
+# 42 Introduction to variables
+
+numberofcats = 10
+numberofcats =numberofcats * 2
+print(numberofcats)
+numberofcats = 99
+print(numberofcats)
+numberofcats = numberofcats -1
+print(numberofcats)
+friends = 0
+print(friends)
+friends = numberofcats
+print(friends)
+
+#==========================================================================
+
+cash = 19867324678987.99
+print(cash/5)
+cash = cash / 5
+print(cash)
+
+#==========================================================================
+"""     # 44 Variable Naming Restrictions and conventions
+
+        # 1 . Variavbles must stat with a letter or underscore    
+        ## ex : _cats , cats
+        #2 . The rst of the name must consist of letters,numbers or Underscores
+        ## cats is fine  but hey@you not fine 
+        #3 Names are case-Sensitive
+        ## CATS != Cats ,  Cats != cats
+
+     Naming Conventions
+        #CamelCase
+        #shake_case
+      Most python programmers prefer to use standard style conventions when naming 
+        things :
+      Most variables should be snake_case (Underscores between words )
+     Most variables should be also be lowercase with some exceptions:
+        #CAPTAL_SNAKE_CASE usually refers to contants (eg PI=3.14)
+        #UpperCamelCase Usually Refers to a class 
+     Variables that start and end with two underscores (called "Dunder for double underscore")
+        are supposed to be private to be private or left alone
+
+                _no_touchy_
+    
+ """
+
+
+#==========================================================================
+
+            #45  DATA TYPES OVERVIEW
+"""     
+    1> Pyhon DATAtypes includes :
+
+        bool : true or false values
+        int  : an integer (1,2,3)
+        str  : string a sequence if unicode eg "Susil How are you"
+        list : an orfered sequence of values of other data types ed [1,2,3]
+        dict : a collection of key : values eg {"first_name":"susil","last_name":"kumar"}
+    
+  """
+
+ #==========================================================================
+
+                 # 46 WHAT THE HECK IS DYNAMIC TYPING 
+
+    
